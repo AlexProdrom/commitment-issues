18cce35565ccebec240a9a5f2687fedf19c23d2c
AlexProdrom <prodromalex@yahoo.com>
2018-12-02 21:28:04 +0100

    1

--- commitments.bat
+++ commitments.bat
@@ -4,6 +4,5 @@ call git add test.txt
 FOR /L %%P IN (1,1,200) DO (
 echo wow >> test.txt
 call git add --all
-call git commit -m"1")
-
-call git push origin master
+call git commit -m"1"
+call git push origin master)
