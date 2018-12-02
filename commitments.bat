47fdc2ce6e60a61d2a9bdfa780da1b9b3378efc7
AlexProdrom <prodromalex@yahoo.com>
2018-12-02 21:23:08 +0100

    loop added to bat file

--- commitments.bat
+++ commitments.bat
@@ -1,9 +1,9 @@
 echo > test.txt
 call git add test.txt
 
+FOR /L %%P IN (1,1,200) DO (
 echo wow >> test.txt
 call git add --all
-call git commit -m"1"
-pause
+call git commit -m"1")
 
 call git push origin master
